5e86af9bc9764056b91c3e69c2a966cccf3b8a03
elvis-chuks <celvischuks@gmail.com>
2019-12-03 18:06:51 +0100

    minor fixes

--- handlers/zroute.go
+++ handlers/zroute.go
@@ -1,43 +1,40 @@
 package handlers
 
-
-
 import (
 	"database/sql"
 	"fmt"
 	"log"
 
-	_ "github.com/denisenkom/go-mssqldb"
 	"encoding/json"
 	"net/http"
-	"../models"
 
+	"../models"
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
-
-
 func zrouteData(r string) models.FullResp {
-	
-	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
-		server,user,password,port,database)
 
-	conn,err := sql.Open("mssql",connString)
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		server, user, password, port, database)
+
+	conn, err := sql.Open("mssql", connString)
 
 	if err != nil {
-		log.Fatal("Open connection failed:",err.Error())
+		log.Fatal("Open connection failed:", err.Error())
 	}
 
 	db := conn
-	
-	tsql := fmt.Sprintf( `
+
+	// sql query to get the routeid and dru from the zroute table
+	tsql := fmt.Sprintf(`
 	SELECT zrouteid,dru
 	FROM dbo.zroute
 	WHERE oru = '%s';
-	`,r)
+	`, r)
 
-	rows,err := db.Query(tsql)
+	rows, err := db.Query(tsql)
 
-	if err != nil{
+	if err != nil {
 		fmt.Println("Error reading rows")
 	}
 
@@ -46,47 +43,46 @@ func zrouteData(r string) models.FullResp {
 	var res []models.Resp
 
 	var tempRes []models.Resp
-	
-	for rows.Next() {
-		var id,dru string
-		err := rows.Scan(&id,&dru)
 
-		if err != nil{
+	for rows.Next() {
+		var id, dru string
+		err := rows.Scan(&id, &dru)
+
+		if err != nil {
 			fmt.Println("Error looping rows")
 		}
-		payload := models.Resp{"id":id,"dru":dru}
-		tempRes = append(tempRes,payload)
+		payload := models.Resp{"id": id, "dru": dru} // create map to hold id,dru (key,value) pairs
+		// append the payload to the array of model.Resp
+		tempRes = append(tempRes, payload)
 	}
-
-	for _,i := range tempRes {
+	// iterating through the tempRes array and using the dru value to query the database
+	for _, i := range tempRes {
+		// another query
 		tsql = fmt.Sprintf(`
 			SELECT depotname
 			FROM dbo.depot
 			WHERE depotcode = '%s';
-			`,i["dru"])
-		
-			rows,err := db.Query(tsql)
+			`, i["dru"])
 
-			if err != nil{
-				fmt.Println("Error reading rows")
-			}
-		
-			defer rows.Close()
+		rows, err := db.Query(tsql)
 
-			
+		if err != nil {
+			fmt.Println("Error reading rows")
+		}
 
-			for rows.Next() {
-				var name string
-				err := rows.Scan(&name)
-				if err != nil {
-					fmt.Println("error looping through rows")
-				}
+		defer rows.Close()
 
-				resp := models.Resp{"routeid":i["id"],"routename":name}
-				res = append(res,resp)
-
+		for rows.Next() {
+			var name string
+			err := rows.Scan(&name)
+			if err != nil {
+				fmt.Println("error looping through rows")
 			}
 
+			resp := models.Resp{"routeid": i["id"], "routename": name}
+			res = append(res, resp)
+
+		}
 
 	}
 
@@ -99,13 +95,11 @@ func zrouteData(r string) models.FullResp {
 
 }
 
-func Zroute(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("content-type","application/json")
-
-	
+func Zroute(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
 
 	depotcode := r.Header["Depotcode"][0]
-	
+
 	// check if depotcode is empty
 
 	data := zrouteData(depotcode)
